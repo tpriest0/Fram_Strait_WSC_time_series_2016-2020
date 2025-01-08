29970ffe392e75df1fc966720b7f9d7fc3806ed9
Taylor Priest <60430926+tpriest0@users.noreply.github.com>
2025-01-08 09:40:58 +0100

    Update FRAM_RAS_F4_time_series_oscillation_signal_analysis.R

--- Scripts/FRAM_RAS_F4_time_series_oscillation_signal_analysis.R
+++ Scripts/FRAM_RAS_F4_time_series_oscillation_signal_analysis.R
@@ -1,18 +1,17 @@
 ####################
 
-#### Processing time-series analysis output
+#### Processing oscillation signal data
 
 ####################
 
 ### Define working directory
-setwd('XXXXX')
+setwd('XXXXXX')
 
 ### Define output directories
 output_figures = ('results/figures/')
 output_tables = ('results/tables/')
 
 ### Load libraries
-
 library(stringr)
 library(dplyr)
 library(ggplot2)
@@ -21,29 +20,19 @@ library(tibble)
 library(patchwork)
 library(data.table)
 
-### Import files
-options(scipen=999)
+### Import oscillation tables
+gene_clust_oscillations = read.table(paste0(output_tables,"FRAM_RAS_F4_GENE_CLUST_oscillations_per_year.txt"), sep="\t", header=T)
+euk_asv_oscillations = read.table(paste0(output_tables,"FRAM_RAS_F4_EUK_oscillations_per_year.txt"), sep="\t", header=T)
+prok_asv_oscillations = read.table(paste0(output_tables,"FRAM_RAS_F4_MIC_oscillations_per_year.txt"), sep="\t", header=T)
 
-### Import oscillation information
-gene_clust_oscillations = read.table("FRAM_RAS_F4_GENE_CLUST_oscillations_per_year.txt", sep="\t", header=T)
-euk_asv_oscillations = read.table("FRAM_RAS_F4_EUK_oscillations_per_year.txt", sep="\t", header=T)
-prok_asv_oscillations = read.table("FRAM_RAS_F4_PROK_oscillations_per_year.txt", sep="\t", header=T)
-
-### Import relative abundance information
-# Import microbial ASV relative abundance data
-mic_asv_rel=read.table(file=paste0(output_tables,"FRAM_RAS_F4_PROK_ASV_filt_rel.txt"), sep="\t",
+### Import relative abudnance tables
+mic_asv_rel=read.table(file=paste0(output_tables,"FRAM_RAS_F4_MIC_ASV_filt_rel.txt"), sep="\t",
                        check.names=F, header=T, row.names=1)
-
-# Import EUK ASV relative abundance data
 euk_asv_rel=read.table(file=paste0(output_tables,"FRAM_RAS_F4_EUK_ASV_filt_rel.txt"), sep="\t",
                        check.names=F, header=T, row.names=1)
-
-# Import gene clusters relative abundance data
 gene_clust_rel=read.table(
   file=paste0(output_tables,"FRAM_RAS_F4_GENE_CLUST_filt_rel_wide.txt"), 
   sep="\t", check.names=F, header=T, row.names=1)
-
-#####
 
 ### Combine oscillation from asvs and gene clusters together
 asv_and_geneclust_oscillations = rbind(gene_clust_oscillations,euk_asv_oscillations,prok_asv_oscillations) %>%
@@ -52,12 +41,6 @@ asv_and_geneclust_oscillations = rbind(gene_clust_oscillations,euk_asv_oscillati
         grepl("euk",nodeId) ~ "Euk",
         grepl("gene",nodeId) ~ "Gene_clust"
     ))
-
-#####
-
-### Process and filter the time-series output
-
-#####
 
 ### Assess the oscillation signal data retrieved from the time-series analysis
 # Calculate the mean oscillations per year
@@ -172,16 +155,10 @@ plot_asv_gene_mean_osc_per_year <- plot_num_oscillations_py(asv_and_geneclust_os
 plot_asv_gene_rel_prop_per_osc <- plot_rel_prop_per_oscillation(asv_gene_rel_prop_per_oscillation)
 
 # Export figure
-pdf(file=paste0(output_figures,"FRAM_RAS_F4_PROK_EUK_ASV_GENE_OSC_per_year_and_rel_abund.pdf"),
+pdf(file=paste0(output_figures,"FRAM_RAS_F4_MIC_EUK_ASV_GENE_OSC_per_year_and_rel_abund.pdf"),
     height=10, width=8)
 plot_asv_gene_mean_osc_per_year|plot_asv_gene_rel_prop_per_osc
 dev.off()
-
-#####
-
-### Analyse the ASVs and GENEs with one oscillation per year
-
-#####
 
 ### Create a list of ASVs and genes that exhibit one oscillation per year year
 mic_euk_asv_gene_clust_osc4_id_list = 
@@ -189,11 +166,8 @@ mic_euk_asv_gene_clust_osc4_id_list =
   filter(., oscillations == 1) %>%
   select(nodeId)
 
-### Create relative abundance (ASV) and normalised count (GENEs)
-### dataframes for those with one oscillation per year as these will be 
+### Create relative abundance dataframes for those with one oscillation per year as these will be 
 ### used as the input for network analysis
-### The EUK ASVs will not be used for network analysis, but we will still create
-### the relevant dataframes for other comparative analysis
 mic_asv_osc4_rel_wide = 
   mic_asv_rel %>%
   tibble::rownames_to_column(., var="nodeId") %>%
@@ -211,10 +185,10 @@ gene_clust_osc4_rel_wide =
 
 # Export tables
 write.table(mic_euk_asv_gene_clust_osc4_id_list,
-            file=paste0(output_tables,"FRAM_RAS_F4_PROK_EUK_ASVs_GENE_CLUST_OSC4_ID_list.txt"),
+            file=paste0(output_tables,"FRAM_RAS_F4_MIC_EUK_ASVs_GENE_CLUST_OSC4_ID_list.txt"),
             sep="\t", quote = F, row.names = F)
 write.table(mic_asv_osc4_rel_wide,
-            file=paste0(output_tables,"FRAM_RAS_F4_PROK_ASV_OSC4_rel_abund_wide.txt"),
+            file=paste0(output_tables,"FRAM_RAS_F4_MIC_ASV_OSC4_rel_abund_wide.txt"),
             sep="\t", quote = F, row.names = F)
 write.table(euk_asv_osc4_rel_wide,
             file=paste0(output_tables,"FRAM_RAS_F4_EUK_ASV_OSC4_rel_abund_wide.txt"),
@@ -222,6 +196,44 @@ write.table(euk_asv_osc4_rel_wide,
 write.table(gene_clust_osc4_rel_wide,
             file=paste0(output_tables,"FRAM_RAS_F4_GENE_CLUST_OSC4_rel_abund_wide.txt"),
             sep="\t", quote = F, row.names = F)
+
+#####
+
+### Group gene clusters with single annual oscillations into functional clusters
+
+#####
+
+### Import gene cluster to function information
+gene_to_func_to_tax=read.table(
+  file="FRAM_RAS_F4_GENE_CLUST_to_FUNC_to_taxonomy.txt",
+  sep="\t",check.names=F, header=T)
+
+# Filter the gene cluster to functional information to retain only gene clusters
+# with a single oscillation per year, then combine with relative abundance matrix and
+# sum the abundances of gene clusters for each function in each sample. This will
+# generate an abundance profile for the functional clusters
+func_osc4_rel_wide = gene_clust_osc4_rel_wide %>%
+  filter(nodeId %in% gene_to_func_to_tax$Gene_clustID) %>%
+  left_join(gene_to_func_to_tax, by=c("nodeId" = "Gene_clustID")) %>%
+  select(-nodeId,-GTDB_taxonomy,-Domain_tiara,-FUNC,-Gene) %>%
+  reshape2::melt(., id.vars=c("funcID"), variable.name="RAS_id",
+                 value.name="Rel_abund") %>%
+  aggregate(Rel_abund~funcID+RAS_id, data=., FUN=sum) %>%
+  reshape2::dcast(funcID~RAS_id, value.var="Rel_abund", data=.)
+
+# The functional profile will now be subject to fourier transformation and then
+# combined with the prokaryotic ASV oscillations in a network analysis
+write.table(func_osc4_rel_wide,
+            file=paste0(output_tables,"FRAM_RAS_F4_FUNC_osc4_rel_wide.txt"),
+            sep="\t", quote = F, row.names = F)
+
+
+#####
+
+### Determine the annual peaks of oscillation signals for those with an oscillation of once per year
+
+#####
+
             
 #####
 
@@ -233,7 +245,7 @@ library(lubridate)
 
 # Import table containing information about the sampling time point when
 # each annually oscillating ASV and gene cluster reached peak abundance each year
-mic_asv_gene_osc4_peaks_per_year_julian=read.table(file="FRAM_RAS_F4_PROK_ASV_GENES_OSC4_peak_timings_julian.txt", 
+mic_asv_gene_osc4_peaks_per_year_julian=read.table(file="RAS_F4_MIC_ASV_GENES_OSC4_peak_timings_julian.txt", 
                                   sep="\t", check.names=F, header=T) 
 
 # Using the information, convert the peak sample dates to julian day
@@ -267,47 +279,3 @@ mic_asv_gene_osc_within_30_days %>%
   )) %>%
   select(type) %>%
   table()
-
-####################
-
-#####
-### Process microbial gene time-series data ready for network input
-### A correlation network will be constructed based on fourier transformed
-### information. We are specifically interested in ASVs/genes
-### with a single oscillation per year. 
-### However, because there are too many genes to include in a network,
-### the orthologous genes will be grouped based on functional annotations
-### We have used the EGGNOG seed ortholog annotations. Where KEGG was available
-### this was the priority, followed by PFAM.
-#####
-
-####################
-
-### Import files
-
-# Import EGGNOG ID to geneID to function information
-gene_to_func_to_tax=read.table(
-  file="FRAM_RAS_F4_proteins_all_cluster_reps_filt_gene_to_func_to_taxonomy.txt",
-  sep="\t",check.names=F, header=T)
-
-# How many gene clusters were assigned a function based on eggnog database?
-dim(gene_to_func_to_tax)
-
-# Filter the gene cluster to functional information to retain only gene clusters
-# with a single oscillation per year, then combine with relative abundance matrix and
-# sum the abundances of gene clusters for each function in each sample. This will
-# generate an abundance profile for the functional clusters
-func_osc4_rel_wide = gene_clust_osc4_rel_wide %>%
-  filter(nodeId %in% gene_to_func_to_tax$Gene_clustID) %>%
-  left_join(gene_to_func_to_tax, by=c("nodeId" = "Gene_clustID")) %>%
-  select(-nodeId,-GTDB_taxonomy,-Domain_tiara,-FUNC,-Gene) %>%
-  reshape2::melt(., id.vars=c("funcID"), variable.name="RAS_id",
-                 value.name="Rel_abund") %>%
-  aggregate(Rel_abund~funcID+RAS_id, data=., FUN=sum) %>%
-  reshape2::dcast(funcID~RAS_id, value.var="Rel_abund", data=.)
-
-# The functional profile will now be subject to fourier transformation and then
-# combined with the prokaryotic ASV oscillations in a network analysis
-write.table(func_osc4_rel_wide,
-            file=paste0(output_tables,"FRAM_RAS_F4_FUNC_osc4_rel_wide.txt"),
-            sep="\t", quote = F, row.names = F)
